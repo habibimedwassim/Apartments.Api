76836d161ff6e18893002c5df9bb5c805cf0c698
Habibi Med Wassim <habibimedwassim@gmail.com>
2024-08-16 20:23:22 +0100

    Fixed decimal precision bug

---
+++ RenTN.Application/DTOs/IdentityDTO/RegisterDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace RenTN.Application.DTOs.IdentityDTO;
+
+public class RegisterDTO
+{
+    public string Email { get; set; } = default!;
+    public string UserName { get; set; } = default!;
+    public string Password { get; set; } = default!;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+}
