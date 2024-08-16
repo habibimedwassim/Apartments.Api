c8588a95bd18d86cb071ecace48b1bf619081c37
Habibi Med Wassim <habibimedwassim@gmail.com>
2024-08-16 23:53:26 +0100

    Added login/register/verifyEmail including jwt and email service

---
+++ RenTN.Application/Services/EmailService/EmailService.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Mail;
+using System.Net;
+using Microsoft.Extensions.Options;
+using RenTN.Domain.Common;
+
+namespace RenTN.Application.Services.EmailService;
+
+public class EmailService(IOptions<SmtpSettings> smtpSettings) : IEmailService
+{
+    private readonly SmtpSettings _smtpSettings = smtpSettings.Value;
+
+    public async Task SendEmailAsync(string to, string subject, string body)
+    {
+        var smtpClient = new SmtpClient(_smtpSettings.Host)
+        {
+            Port = _smtpSettings.Port,
+            Credentials = new NetworkCredential(_smtpSettings.Username, _smtpSettings.Password),
+            EnableSsl = true,
+        };
+
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress(_smtpSettings.From),
+            Subject = subject,
+            Body = body,
+            IsBodyHtml = true,
+        };
+        mailMessage.To.Add(to);
+
+        await smtpClient.SendMailAsync(mailMessage);
+    }
+}
