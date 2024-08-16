be9a37ca5d5872299fb4201c64293e6db22cb46b
Habibi Med Wassim <habibimedwassim@gmail.com>
2024-08-16 17:24:07 +0100

    Added serilog

--- RenTN.API/Extensions/WebApplicationBuilderExtensions.cs
+++ RenTN.API/Extensions/WebApplicationBuilderExtensions.cs
@@ -8,32 +8,32 @@ public static class WebApplicationBuilderExtensions
 {
     public static void AddPresentation(this WebApplicationBuilder builder)
     {
-        //builder.Services.AddAuthentication();
+        builder.Services.AddAuthentication();
         builder.Services.AddControllers();
-        //builder.Services.AddSwaggerGen(c =>
-        //{
-        //    c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
-        //    {
-        //        Type = SecuritySchemeType.Http,
-        //        Scheme = "Bearer"
-        //    });
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "Bearer"
+            });
 
-        //    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-        //    {
-        //        {
-        //            new OpenApiSecurityScheme
-        //            {
-        //                Reference = new OpenApiReference {Type=ReferenceType.SecurityScheme, Id = "bearerAuth"}
-        //            },
-        //            []
-        //        }
-        //    });
-        //});
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference {Type=ReferenceType.SecurityScheme, Id = "bearerAuth"}
+                    },
+                    []
+                }
+            });
+        });
 
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddScoped<ErrorHandlingMiddleware>();
 
-        //builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
+        builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
     }
 }
