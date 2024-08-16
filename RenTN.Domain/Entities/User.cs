8ea22e4c1b1775bceefbd992a7d08a72308d4151
Habibi Med Wassim <habibimedwassim@gmail.com>
2024-08-16 20:23:22 +0100

    Fixed decimal precision bug

--- RenTN.Domain/Entities/User.cs
+++ RenTN.Domain/Entities/User.cs
@@ -6,5 +6,7 @@ public class User : IdentityUser
 {
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
+    public string? VerificationCode { get; set; }
+    public DateTime? VerificationCodeExpiration { get; set; }
     public List<Apartment> OwnedApartments { get; set; } = [];
 }
