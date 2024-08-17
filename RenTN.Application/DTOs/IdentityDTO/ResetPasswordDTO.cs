03d3900057d5ad0ec46b6bd68a8f208d45ba2f29
Habibi Med Wassim <habibimedwassim@gmail.com>
2024-08-17 11:22:06 +0100

    Completed the auth logic

---
+++ RenTN.Application/DTOs/IdentityDTO/ResetPasswordDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace RenTN.Application.DTOs.IdentityDTO;
+
+public class ResetPasswordDTO
+{
+    public string Email { get; set; } = default!;
+    public string VerificationCode { get; set; } = default!;
+    public string NewPassword { get; set; } = default!;
+}
